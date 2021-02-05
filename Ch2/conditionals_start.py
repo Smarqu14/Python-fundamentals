dd9b35945cb3c7945feb7328d842e35a99b1875a
Steve Marquez <steveantonio00@gmail.com>
2021-02-05 15:44:30 -0800

    Finish conditional structures section in chapter 2

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -4,11 +4,24 @@
 
 
 def main():
-    x, y = 10, 100
+    x, y = 100, 10
 
+    # Doesnt have switch statements
     # conditional flow uses if, elif, else
+    if (x < y):
+        st = 'x is less than y'
+    elif (x == y):
+        st = 'X is greater than y'
+    else:
+        st = "They are greater"
 
+    print(st)
+
+# Ternary condioning
     # conditional statements let you use "a if C else b"
+    st = 'X is less than Y' if (
+        x < y) else "x is greater than or the same as y"
+    print(st)
 
 
 if __name__ == "__main__":
