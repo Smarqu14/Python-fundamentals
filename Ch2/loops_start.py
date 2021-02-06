ff2960aa416fe5a0e450d16e8bd9e4e17b317c26
Steve Marquez <steveantonio00@gmail.com>
2021-02-05 16:05:38 -0800

    Finish Loops section in chapter 2

--- Ch2/loops_start.py
+++ Ch2/loops_start.py
@@ -3,21 +3,38 @@
 #
 
 def main():
-  x = 0
+    x = 0
 
-  # define a while loop
+    # define a while loop
+    while(x < 5):
+    print(x)
+    x = x+1
+
+    # define a for loop
+    for x in range(5, 10):
+        print(x)
+
+    # # use a for loop over a collection
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
+    for d in days:
+        print(d)
+
+    # use the break and continue statements
+    for x in range(5, 10):
+        # if (x == 7):
+        #     break
+        if (x % 2 == 0):
+            continue
+        print(x)
+
+    # using the enumerate() function to get index
+
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
+    for index, item in enumerate(days):
+        print(index, item)
 
 
-  # define a for loop
-
-
-  # use a for loop over a collection
-
-  
-  # use the break and continue statements
-
-
-  #using the enumerate() function to get index 
-  
 if __name__ == "__main__":
-  main()
+    main()
